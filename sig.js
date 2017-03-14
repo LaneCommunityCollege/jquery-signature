96c5d0e1a91523df1785f56e1801f2bc9c45260b
Kyle Schmidt <schmidtk@lanecc.edu>
2017-03-13 23:11:59 -0700

    got errors clearing correctly

--- sig.js
+++ sig.js
@@ -1,3 +1,5 @@
+$.fn.clearValidation = function(){var v = $(this).validate();$('[name]',this).each(function(){v.successList.push(this);v.showErrors();});v.resetForm();v.reset();};
+
 $(function(){
 	var form = $('form');
 
@@ -7,18 +9,29 @@ $(function(){
 	  		var target = "." + this.id + ".field";
 	  		$(target).text($(this).val());
 
+	  		if(this.id == "url"){
+	  			$('.field.url').attr('href', $(this).val())
+	  		}
+
+	  		//if both email and phone are set, then add a pipe
 	  		if($('.field.email').text() && $('.field.phone').text() && !$('.mid').text()){
 	  			$('.mid').text(" | ");
 	  		}
 	  		else {
 	  			$('.mid').text("");
 	  		}
+
+	  		if($(this).val() == ""){
+	  			$(this).clearValidation();
+	  		}
 	  	}
 	  });
 	});
 
-	$.validator.addMethod("emailRegex", function(value, element, regexpr) {          
-    return regexpr.test(value);
+	$.validator.addMethod("emailRegex", function(value, element, regexpr) { 
+	   if(value.length > 0)         
+    	return regexpr.test(value);
+     return true;
 	}, "Be sure to use your Lane email address");
 
 	$("form").validate({
@@ -31,7 +44,13 @@ $(function(){
 			},
 			phone: {
 				phoneUS: true
+			},
+			url: {
+				url: true
 			}
+		},
+		messages: {
+			url: "Please enter a valid URL, starting with http://"
 		}
 	});
 });
