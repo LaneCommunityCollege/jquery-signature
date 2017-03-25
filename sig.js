8e2dd89252270c28813bb083ac0d26294bda972d
Kyle Schmidt <schmidtk@lanecc.edu>
2017-03-24 23:16:34 -0700

    working increasing size

--- sig.js
+++ sig.js
@@ -1,5 +1,11 @@
 $.fn.clearValidation = function(){var v = $(this).validate();$('[name]',this).each(function(){v.successList.push(this);v.showErrors();});v.resetForm();v.reset();};
 
+function resizePic(){
+  var h = $('tbody').height();
+  $('#signature img').attr('height', h + "px").attr('width', h + "px");
+  $('#signature img').parents('td').css('width', h + Math.floor(h/9) + "px");
+}
+
 function previewPicture(){
   var preview = document.querySelector('#signature img');
   var file    = document.querySelector('input[type=file]').files[0];
@@ -14,7 +20,7 @@ function previewPicture(){
   }
 
   $('#btn-txt').text('Picture added!');
-  $('#signature img').parents('td').css('width', '85px');
+  resizePic();
   $('#signature img').css("display", "block");
 }
 
@@ -27,8 +33,6 @@ function copyText(element) {
   selection.addRange(range);
   document.execCommand('copy');
 }
- 
-
 
 $(function(){
   var form = $('form');
@@ -40,14 +44,25 @@ $(function(){
       $('#btn-txt').text('Add a picture');
 
       $('#signature img').parents('td').css('width', '0');
-      $('#signature img').attr("src", "");
+      $('#signature img').attr("src", "").removeAttr("height width");
       $('#signature img').css("display", "none");
     })
 
     $('input').on('keyup paste blur', function(){
       if($(this).valid() || $(this).val() == ""){
         var target = "." + this.id + ".field";
+        
+        //if we're adding text that wasn't previously there, or our new value is empty
+        //resize the picture
+        //TODO if we're working with a hidden field, this logic doesn't work.
+        var resize = false;
+        if($(this).val() != $(target).text() && !$(this).hasClass('notshown') && ($(target).text() == "" || $(this).val() == "")){
+          resize = true;
+        }
         $(target).text($(this).val());
+        if(resize){
+          resizePic();
+        }
 
         if(this.id == "url"){
           if(this.value && $('.field.department a').length)
